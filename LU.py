4de9a8b3b28565b1cde6ae8f8e8e101896dc10c6
Rmin-Sadigh <armin_sadigh@yahoo.com>
2020-05-06 14:44:22 +0430

    commenting and index change

--- LU.py
+++ LU.py
@@ -1,3 +1,4 @@
+# ─── LIBRARIES ──────────────────────────────────────────────────────────────────
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import trange
@@ -5,8 +6,9 @@ import os
 import PIL
 import csv
 
+# ─── CLEAN CONSOLE ──────────────────────────────────────────────────────────────
 os.system("cls")
-
+# ─── IMPORT LANDUSE COLORS ──────────────────────────────────────────────────────
 RgbCodes = dict()
 RgbCodes[0] = np.array([255, 255, 255])
 csvfile = open("data/RgbCodes.txt", "rt")
@@ -14,30 +16,30 @@ reader = csv.reader(csvfile, delimiter="\t", skipinitialspace=True)
 for num in reader:
     RgbCodes[int(num[0])] = np.array([int(num[1]), int(num[2]), int(num[3])])
 csvfile.close()
-
+# ─── IMPORT IMAGE ───────────────────────────────────────────────────────────────
 img = PIL.Image.open("data/UL_Crop.png")
 imgRaster = np.asarray(img)
 classifiedRaster = np.tile(0, (img.height, img.width))
 img.close()
-
+# ─── CLASSIFYING THE IMAGE ──────────────────────────────────────────────────────
 for row in trange(0, imgRaster.shape[0], desc="Reclassifying the image", unit=" rows"):
     for col in range(0, imgRaster.shape[1]):
         for id, color in RgbCodes.items():
             if np.array_equal(imgRaster[row][col], color):
                 classifiedRaster[row][col] = id
                 break
-
+# ─── CREATING RANGE MATRIX TEMPLATE ─────────────────────────────────────────────
 rangeMatrix = np.tile(0, (8, 17, 17))
-for i in range(0, 8):
-    rangeMatrix[i, 8 - i - 1, 8 - i - 1 : 8 + i + 2] = 1
-    rangeMatrix[i, 8 + i + 1, 8 - i - 1 : 8 + i + 2] = 1
-    rangeMatrix[i, 8 - i - 1 : 8 + i + 2, 8 - i - 1] = 1
-    rangeMatrix[i, 8 - i - 1 : 8 + i + 2, 8 + i + 1] = 1
-
+for d in range(0, 8):
+    rangeMatrix[d, 8 - d - 1, 8 - d - 1 : 8 + d + 2] = 1
+    rangeMatrix[d, 8 + d + 1, 8 - d - 1 : 8 + d + 2] = 1
+    rangeMatrix[d, 8 - d - 1 : 8 + d + 2, 8 - d - 1] = 1
+    rangeMatrix[d, 8 - d - 1 : 8 + d + 2, 8 + d + 1] = 1
+# ─── CALCULATING AVERAGE LANDUSE COVER ──────────────────────────────────────────
 KAvgTotal = np.empty([8])
-for i in range(0, 8):
-    KAvgTotal[i] = np.count_nonzero(classifiedRaster == i + 1) / classifiedRaster.size
-
+for k in range(0, 8):
+    KAvgTotal[k] = np.count_nonzero(classifiedRaster == k + 1) / classifiedRaster.size
+# ─── CALCULATING KD FOR EACH CELL ──────────────────────────────────────────────
 IKDLRes = np.zeros((classifiedRaster.shape[0], classifiedRaster.shape[1], 8, 8))
 lCells = classifiedRaster != 0
 for row in trange(
@@ -45,38 +47,38 @@ for row in trange(
 ):
     for col in range(0, classifiedRaster.shape[1]):
         if lCells[row, col]:
-            for i in range(0, 8):
+            for d in range(0, 8):
                 baseImg = classifiedRaster[
-                    max(0, row - i - 1) : 1
-                    + min(row + i + 1, classifiedRaster.shape[0]),
-                    max(0, col - i - 1) : 1
-                    + min(col + i + 1, classifiedRaster.shape[1]),
+                    max(0, row - d - 1) : 1
+                    + min(row + d + 1, classifiedRaster.shape[0]),
+                    max(0, col - d - 1) : 1
+                    + min(col + d + 1, classifiedRaster.shape[1]),
                 ]
                 mask = rangeMatrix[
-                    i,
-                    max(8 - row, 8 - i - 1) : 9
-                    + min(i + 1, classifiedRaster.shape[0] - row - 1),
-                    max(8 - col, 8 - i - 1) : 9
-                    + min(i + 1, classifiedRaster.shape[1] - col - 1),
+                    d,
+                    max(8 - row, 8 - d - 1) : 9
+                    + min(d + 1, classifiedRaster.shape[0] - row - 1),
+                    max(8 - col, 8 - d - 1) : 9
+                    + min(d + 1, classifiedRaster.shape[1] - col - 1),
                 ]
                 maskedRaster = np.multiply(baseImg, mask)
                 for k in range(0, 8):
                     kCount = np.count_nonzero(maskedRaster == k + 1)
                     dTotal = np.count_nonzero(mask == 1)
                     KDAvg = kCount / dTotal
-                    IKDLRes[row, col, k, i] = KDAvg / KAvgTotal[k]
-
+                    IKDLRes[row, col, k, d] = KDAvg / KAvgTotal[k]
+# ─── CALCULATING AVERAGE EF FOR EACH L PER K ────────────────────────────────────
 sumMatrix = np.zeros([6, 8, 8])
-for i in range(0, 6):
-    cellPos = classifiedRaster == i + 1
+for l in range(0, 6):
+    cellPos = classifiedRaster == l + 1
     cellCount = 0
     for row in range(0, classifiedRaster.shape[0]):
         for col in range(0, classifiedRaster.shape[1]):
             if cellPos[row, col]:
                 cellCount += 1
-                sumMatrix[i] += IKDLRes[row, col, :, :]
-    sumMatrix[i] /= cellCount
-
+                sumMatrix[l] += IKDLRes[row, col, :, :]
+    sumMatrix[l] /= cellCount
+# ─── PLOTTING KD FOR EACH L ─────────────────────────────────────────────────────
 if not os.path.exists("data/outputs"):
     os.makedirs("data/outputs")
 for l in trange(0, 6, desc="Printing plots", unit=" Plot"):
